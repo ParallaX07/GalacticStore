b8efdd5c20425d0198ab011f357d79f2cb82c196
TaniaHussain <108230308+TaniaHussain@users.noreply.github.com>
2023-05-24 19:40:33 +0600

    Initial

---
+++ src/Execution.java
@@ -0,0 +1,12 @@
+package GalacticStore;
+
+public class Execution {
+    public static void main(String[] args) {
+        
+        while(true){
+            // ask for email and password
+
+            //add try catch to match email and password
+        }
+    }
+}
